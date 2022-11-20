da2930c6fca4a7132c71a3d668a33adac27a1da1
NataManatu <vip.aleksa78@mail.ru>
2022-11-20 10:35:42 +0300

    DRon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1400,7 +1400,8 @@ const butVect2 = (() => {
                         <li>Работает ли контрруление на низкой скорости?</li>
                         <li>Предоставляете ли вы мотоцикл для сдачи экзамена в ГИБДД?</li>
                         <li id="li-lh-3">Выдаете ли вы экипировку на время занятий на площадке?</li>
-                        <li id="li-lh">Нужна ли регистрация и гражданство,чтобы учиться и сдавать экзамены на категорию ”А”?</li>
+                        <li id="li-lh">Нужна ли регистрация и гражданство,чтобы учиться и сдавать экзамены на категорию
+                            ”А”?</li>
                         <li>Что делать, если не сдал экзамен?</li>
                     </ul>
                 </div>
@@ -1429,9 +1430,71 @@ const butVect2 = (() => {
             </div>
         </div>
 
+        <!-- CONTACT-18-BLOCK -->
+
+        <div class="title-contact">
+            <div class=" title-shadow-cont">
+                <img src="@/assets/img-photo-gal-bl-7/Line 11.svg" alt="" id="about-gallery_3">
+                <p>контакты</p>
+            </div>
+        </div>
 
 
+        <div class="container-contact">
 
+            <div class="contact-content">
+                <div class="header-menu">
+                    <div class="logo">
+                        <img src="@/assets/img-header/logo.svg" alt="">
+                    </div>
+
+                    <div class="nav">
+                        <ul>
+                            <li><a href="#course">Курсы</a></li>
+                            <li><a href="#news">Новости</a> </li>
+                            <li><a href="#about">О нас</a></li>
+                            <li><a href="/">Контакты</a></li>
+                            <li><a href="#reviews">Отзывы</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="social">
+                        <a href="/"><img src="@/assets/img-header/whatsapp.svg"></a>
+                        <a href="/"><img src="@/assets/img-header/tg.svg"></a>
+                        <a href="/"><img src="@/assets/img-header/inst.svg"></a>
+                        <a href="/"><img src="@/assets/img-header/vk.svg"></a>
+                    </div>
+
+                    <div class="tel">
+                        <a href="tel:+79384567707">+7 938 456 77 07</a>
+                    </div>
+                </div>
+
+                <div class="nav-cont">
+                    <p><a href=""><span>Главная</span> </a><a href=""> / Курс “Категория “А”</a></p>
+                </div>
+                <div class="contact-course">
+                    <p id="cont-p-1">Курс “Категория “А”</p>
+                    <p id="cont-p-2">Курс для получения прав категории “А”</p>
+                    <p id="cont-p-3">Предназначен для людей, не умеющих управлять мотоциклом. Обучение проводится на площадке с
+                        удобной локацией. Вы научитесь уверенно обращаться с мотоциклом и сможете гарантированно сдать
+                        практический экзамен в ГИБДД </p>
+                    <ul>
+                        <li>15 часов практики</li>
+                        <li>10 занятий</li>
+                        <li>100% готовность к экзамену</li>
+                    </ul>
+                    <p id="cont-p-4">В стоимость подготовки включены: топливные сборы, экипировка, мотоцикл, услуги инструктора,
+                        дистанционное обучение и внутренний экзамен по теории ПДД.</p>
+
+                        <div class="contact-price">
+                            <p id="cont-p-5">25 000 рублей</p>
+                            <button type="submit"></button>
+                        </div>
+                </div>
+
+            </div>
+        </div>
 
 
 

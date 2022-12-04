82e2235ede570365854472215572c1954a970f60
NataManatu <vip.aleksa78@mail.ru>
2022-12-04 20:09:21 +0300

    DRon

--- src/views/CourseA.vue
+++ src/views/CourseA.vue
@@ -1,70 +1,84 @@
 <template>
 
-        <div class="contact">
-            <div class="container-contact">
-                <div class="contact-content">
-                    <div class="header-menu">
-                        <div class="logo">
-                            <img src="@/assets/img-header/logo.svg" alt="">
-                        </div>
+     
+<body>
 
-                        <div class="nav">
-                            <ul>
-                                <li><a href="#course">Курсы</a></li>
-                                <li><a href="#news">Новости</a> </li>
-                                <li><a href="#about">О нас</a></li>
-                                <li><a href="/">Контакты</a></li>
-                                <li><a href="#reviews">Отзывы</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="social">
-                            <a href="/"><img src="@/assets/img-header/whatsapp.svg"></a>
-                            <a href="/"><img src="@/assets/img-header/tg.svg"></a>
-                            <a href="/"><img src="@/assets/img-header/inst.svg"></a>
-                            <a href="/"><img src="@/assets/img-header/vk.svg"></a>
-                        </div>
-
-                        <div class="tel">
-                            <a href="tel:+79384567707">+7 938 456 77 07</a>
-                        </div>
+    <!-- COURSE-A-БЛОК-1 -->
+    
+    
+        <div class="course-a">
+            <div class="course-a-content">
+                <div class="course-header">
+                    <div class="logo">
+                        <img src="@/assets/img-courseA/logo.svg" alt="">
                     </div>
-
-
-                    <div class="contact-course">
-                        <div class="nav-cont">
-                            <p><a href=""><span>Главная</span> </a><a href=".header-container"> / Курс “Категория
-                                    “А”</a>
-                            </p>
-                        </div>
-                        <div class="cont-content">
-                            <p id="cont-p-1">Курс “Категория “А”</p>
-                            <p id="cont-p-2">Курс для получения прав категории “А”</p>
-                            <p id="cont-p-3">Предназначен для людей, не умеющих управлять мотоциклом. Обучение
-                                проводится на
-                                площадке с
-                                удобной локацией. Вы научитесь уверенно обращаться с мотоциклом и сможете гарантированно
-                                сдать
-                                практический экзамен в ГИБДД </p>
-                            <ul>
-                                <li>15 часов практики</li>
-                                <li>10 занятий</li>
-                                <li>100% готовность к экзамену</li>
-                            </ul>
-                            <p id="cont-p-4">В стоимость подготовки включены: топливные сборы, экипировка, мотоцикл,
-                                услуги
-                                инструктора,
-                                дистанционное обучение и внутренний экзамен по теории ПДД.</p>
-
-                            <div class="contact-price">
-                                <p id="cont-p-5">25 000 рублей</p>
-                                <button type="submit">Записаться</button>
-                            </div>
+    
+                    <div class="nav-course-a">
+                        <ul>
+                            <li><a href="/">Курсы</a></li>
+                            <li><a href="/">Новости</a> </li>
+                            <li><a href="/">О нас</a></li>
+                            <li><a href="/">Контакты</a></li>
+                            <li><a href="/">Отзывы</a></li>
+                        </ul>
+                    </div>
+    
+                    <div class="social">
+                        <a href="/"><img src="@/assets/img-courseA/image01.svg"></a>
+                        <a href="/"><img src="@/assets/img-courseA/image02.svg"></a>
+                        <a href="/"><img src="@/assets/img-courseA/svg.svg"></a>  
+                    </div>
+    
+                    <div class="tel">
+                        <a href="tel:+79384567707">+7 938 456 77 07</a>
+                    </div>
+                </div>
+    
+    
+                <div class="course-card">
+                    <div class="nav-course">
+                        <p><a href=""><span>Главная</span> </a><a href="/"> / Курс “Категория
+                                “А”</a>
+                        </p>
+                    </div>
+                    <div class="about-course">
+                        <p id="cont-p-1">Курс “Категория “А”</p>
+                        <p id="cont-p-2">Курс для получения прав категории “А”</p>
+                        <p id="cont-p-3">Предназначен для людей, не умеющих управлять мотоциклом. Обучение
+                            проводится на
+                            площадке с
+                            удобной локацией. Вы научитесь уверенно обращаться с мотоциклом и сможете гарантированно
+                            сдать
+                            практический экзамен в ГИБДД </p>
+                        <ul>
+                            <li>15 часов практики</li>
+                            <li>10 занятий</li>
+                            <li>100% готовность к экзамену</li>
+                        </ul>
+                        <p id="cont-p-4">В стоимость подготовки включены: топливные сборы, экипировка, мотоцикл,
+                            услуги
+                            инструктора,
+                            дистанционное обучение и внутренний экзамен по теории ПДД.</p>
+    
+                        <div class="course-price">
+                            <p id="cont-p-5">25 000 рублей</p>
+                            <button type="submit">Записаться</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    
+    
+    <!-- THIS YOU CORSE-БЛОК-2 -->
+    
+    
+    
+    
+    
+    
+    
+    </body>
 
 </template>
 

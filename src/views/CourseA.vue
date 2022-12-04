c3b6951093815b827960a6e1dcbf1f0cdda6a698
NataManatu <vip.aleksa78@mail.ru>
2022-12-04 23:58:54 +0300

    DRon

--- src/views/CourseA.vue
+++ src/views/CourseA.vue
@@ -151,7 +151,33 @@
         </div>
 
 
+        <!-- INSTRUCTOR COURSE-БЛОК-5 -->
 
+
+        <div class="instuctor-course-container">
+            <div class="about_instuctor-course">
+                <img src="@/assets/img-about-bl-2/Line 11.svg" alt="">
+                <p>Инструкторы курса</p>
+            </div>
+            <div class="instuctor-course-contant">
+                <div class="instrucors">
+                    <div class="instructor-1">
+                        <img src="@/assets/img-courseA/Rectangle 231.png" alt="">
+                        <div class="in-1">
+                            <img src="@/assets/img-courseA/Rectangle 259.png" alt="">
+                            <p>Терентьев Андрей</p>
+                        </div>
+                    </div>
+                    <div class="instructor-2">
+                        <img src="@/assets/img-courseA/Rectangle 231 (1).png" alt="">
+                        <div class="in-2">
+                            <p>Ефимов Андрей</p>
+                            <img src="@/assets/img-courseA/Rectangle 259 (1).png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 

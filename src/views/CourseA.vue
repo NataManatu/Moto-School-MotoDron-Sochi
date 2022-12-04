4093ae18838793dd527eaa7004b56f4593dc8049
NataManatu <vip.aleksa78@mail.ru>
2022-12-04 21:33:33 +0300

    DRon

--- src/views/CourseA.vue
+++ src/views/CourseA.vue
@@ -101,10 +101,17 @@
             </div>
             <div class="mototeh-course-content">
 
-
-
+                <div class="mototeh-block">
+                    <div class="moto-1">
+                        <img src="@/assets/img-courseA/Image.png" alt="">
+                        <p>BAJAJ Boxer BM 150</p>
+                    </div>
+                    <div class="moto-2">
+                        <img src="@/assets/img-courseA/Image (1).png" alt="">
+                        <p>Yamaha YZF-R3</p>
+                    </div>
+                </div>
             </div>
-
         </div>
 
 

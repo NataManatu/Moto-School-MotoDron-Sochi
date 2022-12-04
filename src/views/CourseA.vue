cdf270cba5a1642132e246a584407f1601ee2985
NataManatu <vip.aleksa78@mail.ru>
2022-12-04 20:35:57 +0300

    DRon

--- src/views/CourseA.vue
+++ src/views/CourseA.vue
@@ -1,18 +1,18 @@
 <template>
 
-     
-<body>
 
-    <!-- COURSE-A-БЛОК-1 -->
-    
-    
+    <body>
+
+        <!-- COURSE-A-БЛОК-1 -->
+
+
         <div class="course-a">
             <div class="course-a-content">
                 <div class="course-header">
                     <div class="logo">
                         <img src="@/assets/img-courseA/logo.svg" alt="">
                     </div>
-    
+
                     <div class="nav-course-a">
                         <ul>
                             <li><a href="/">Курсы</a></li>
@@ -22,19 +22,19 @@
                             <li><a href="/">Отзывы</a></li>
                         </ul>
                     </div>
-    
+
                     <div class="social">
                         <a href="/"><img src="@/assets/img-courseA/image01.svg"></a>
                         <a href="/"><img src="@/assets/img-courseA/image02.svg"></a>
-                        <a href="/"><img src="@/assets/img-courseA/svg.svg"></a>  
+                        <a href="/"><img src="@/assets/img-courseA/svg.svg"></a>
                     </div>
-    
+
                     <div class="tel">
                         <a href="tel:+79384567707">+7 938 456 77 07</a>
                     </div>
                 </div>
-    
-    
+
+
                 <div class="course-card">
                     <div class="nav-course">
                         <p><a href=""><span>Главная</span> </a><a href="/"> / Курс “Категория
@@ -59,7 +59,7 @@
                             услуги
                             инструктора,
                             дистанционное обучение и внутренний экзамен по теории ПДД.</p>
-    
+
                         <div class="course-price">
                             <p id="cont-p-5">25 000 рублей</p>
                             <button type="submit">Записаться</button>
@@ -68,16 +68,63 @@
                 </div>
             </div>
         </div>
-    
-    
-    <!-- THIS YOU CORSE-БЛОК-2 -->
-    
-    
-    
-    
-    
-    
-    
+
+
+        <!-- THIS YOU CORSE-БЛОК-2 -->
+
+        <div class="this-course-container">
+            <div class="about_this-course">
+                <img src="@/assets/img-about-bl-2/Line 11.svg" alt="">
+                <p>Часто задаваемые вопросы</p>
+            </div>
+            <div class="this-course-content">
+
+
+
+
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </body>
 
 </template>
@@ -97,5 +144,5 @@ export default {
 </script>
 
 <style scroped>
-    
+
 </style>

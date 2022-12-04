16aa9b06b86371292062cfc39da0a7d9e8f2d628
NataManatu <vip.aleksa78@mail.ru>
2022-12-04 21:12:50 +0300

    DRon

--- src/views/CourseA.vue
+++ src/views/CourseA.vue
@@ -79,14 +79,33 @@
             </div>
             <div class="this-course-content">
 
+                <div class="course-a-block">
+                    <img src="@/assets/img-courseA/WhoCard01.png" alt="">
+                    <img src="@/assets/img-courseA/WhoCard02.png" alt="">
+                    <img src="@/assets/img-courseA/WhoCard03.png" alt="">
+                    <img src="@/assets/img-courseA/WhoCard04.png" alt="">
+                </div>
 
+                <div class="this-course-photo-block">
+                    <img src="@/assets/img-courseA/Group 260.png" alt="">
+                </div>
+            </div>
+        </div>
 
+        <!-- MOTOTEHN-COURSE-БЛОК-3 -->
+
+        <div class="mototeh-course-container">
+            <div class="about_mototeh-course">
+                <img src="@/assets/img-about-bl-2/Line 11.svg" alt="">
+                <p>мотоТехника курса</p>
+            </div>
+            <div class="mototeh-course-content">
 
 
 
             </div>
-        </div>
 
+        </div>
 
 
 

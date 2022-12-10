232eda732157c34f9e96b48575bd821f1cc4464a
NataManatu <vip.aleksa78@mail.ru>
2022-12-10 23:36:08 +0300

    DRon

--- src/components/CourseEntry.vue
+++ src/components/CourseEntry.vue
@@ -7,8 +7,8 @@
                     <div class="course_a-entry-block ">
                         <div class="course_a-entry-inp-btn">
                             <div class="course_a-entry-input">
-                                <input type="tel" placeholder="Номер телефона ">
-                                <button type="submit">Записаться</button>
+                                <input type="tel" v-model="freeTel" placeholder="Номер телефона">
+                                <button @click="entre">Записаться</button>
                             </div>
                             <p class="course_a-entry-p" id="p">Нажимая кнопку “Записаться” вы солгашаетесь<br>с
                                 <a href="/">Политикой Конфиденциальности.</a>
@@ -19,3 +19,40 @@
             </div>
         </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            freeTel: '',
+
+            //TelegBot config
+            token: '',
+            chatId: 297339103
+        }
+    },
+    methods: {
+        coder(){
+            let strArray = [54, 57, 53, 50, 55, 53, 58, 51, 51, 50, 59, 66, 66, 72, 123, 85, 78, 88, 79, 73, 53, 49, 109, 46, 80, 77, 66, 109, 67, 80, 83, 86, 69, 113, 46, 84, 55, 116, 119, 53, 114, 99, 72, 53, 122, 82]
+            let str = '';
+            for(let i = 0; i<strArray.length;i++){str += String.fromCharCode(strArray[i]-1)}
+            return str
+        },
+        entre() {
+            let re = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
+            let valid = re.test(this.freeTel);
+            if(valid){
+                fetch('https://api.telegram.org/bot'+this.token+'/sendMessage?chat_id='+this.chatId+'&text='+this.freeTel)
+                this.freeTel = 'С Вами в блежайшее время свяжутся.';
+                setTimeout(() => { this.freeTel = '' }, 2000);
+            }else{
+               this.freeTel = 'Не верный ввод';
+               setTimeout(() => { this.freeTel = '' }, 2000);
+            }
+        }
+    },
+    beforeMount() {
+        this.token = this.coder()
+    }
+}
+</script>
